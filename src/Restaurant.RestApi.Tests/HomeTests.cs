4affe3f667b4ab8a968244989ffd1878dc78fb96
Artem Agafonov <agart@mail.ru>
2025-02-04 21:37:54 +0300

    Tests. Suppress warnings for rules CA2007, CA2234.

--- src/Restaurant.RestApi.Tests/HomeTests.cs
+++ src/Restaurant.RestApi.Tests/HomeTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Restaurant.RestApi.Tests;
 
@@ -11,13 +12,17 @@ public class HomeTests
     /// Home returns OK status code.
     /// </summary>
     [Fact]
+    [SuppressMessage(
+        "Usage",
+        "CA2234:Pass system uri objects instead of strings",
+        Justification = "URL isn't passed as variable, but as literal.")]
     public async Task HomeIsOk()
     {
         await using var factory = new WebApplicationFactory<Program>();
         var client = factory.CreateClient();
 
         var response = await client
-            .GetAsync(new Uri("", UriKind.Relative));
+            .GetAsync("");
 
         Assert.True(
             response.IsSuccessStatusCode,
