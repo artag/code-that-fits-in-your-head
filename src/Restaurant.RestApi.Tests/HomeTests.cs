95d072f430120078df87f22f1d62a0393a26dca0
Artem Agafonov <agart@mail.ru>
2025-02-04 19:56:51 +0300

    Add test HomeIsOk.

---
+++ src/Restaurant.RestApi.Tests/HomeTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Restaurant.RestApi.Tests;
+
+/// <summary>
+/// Tests for home resource.
+/// </summary>
+public class HomeTests
+{
+    /// <summary>
+    /// Home returns OK status code.
+    /// </summary>
+    [Fact]
+    public async Task HomeIsOk()
+    {
+        await using var factory = new WebApplicationFactory<Program>();
+        var client = factory.CreateClient();
+
+        var response = await client
+            .GetAsync(new Uri("", UriKind.Relative));
+
+        Assert.True(
+            response.IsSuccessStatusCode,
+            $"Actual status code: {response.StatusCode}.");
+    }
+}
--- src/Restaurant.RestApi/Program.cs
+++ src/Restaurant.RestApi/Program.cs
@@ -1,6 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace Restaurant.RestApi;
 
-app.MapGet("/", () => "Hello World!");
+/// <summary>
+/// Program.
+/// </summary>
+[System.Diagnostics.CodeAnalysis.SuppressMessage(
+    "Design",
+    "CA1052:Static holder types should be Static or NotInheritable",
+    Justification = "Class Program is not static for testing purposes")]
+[System.Diagnostics.CodeAnalysis.SuppressMessage(
+    "Roslynator",
+    "RCS1102:Make class static",
+    Justification = "Class Program is not static for testing purposes")]
+public class Program
+{
+    internal static Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
 
-app.Run();
+        app.MapGet("/", () => "Hello World!");
+
+        return app.RunAsync();
+    }
+}
