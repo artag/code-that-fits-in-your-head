06839af4faf6cae1dcdde96ff8f51687ae792fd1
Artem Agafonov <agart@mail.ru>
2025-02-11 19:41:09 +0300

    Add guard againt negative and zero quantity to the Reservation ctor.

---
+++ src/Restaurant.RestApi.Tests/ReservationTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Restaurant.RestApi.Tests;
+
+public class ReservationTests
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void QuantityMustBePositive(int invalidQuantity)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () => new Reservation(
+                new DateTime(2024, 8, 19, 11, 30, 0),
+                "mail@example.com",
+                "Marie Petrovich",
+                invalidQuantity));
+    }
+}
--- src/Restaurant.RestApi/Reservation.cs
+++ src/Restaurant.RestApi/Reservation.cs
@@ -1,8 +1,26 @@
 ﻿namespace Restaurant.RestApi;
 
-public record Reservation(
-    DateTime At,
-    string Email,
-    string Name,
-    int Quantity
-);
+public record Reservation
+{
+    public Reservation(
+        DateTime at,
+        string email,
+        string name,
+        int quantity)
+    {
+        if (quantity < 1)
+            throw new ArgumentOutOfRangeException(
+                nameof(quantity),
+                "The value must be a positive (non-zero) number.");
+
+        At = at;
+        Email = email;
+        Name = name;
+        Quantity = quantity;
+    }
+
+    public DateTime At { get; init; }
+    public string Email { get; init; }
+    public string Name { get; init; }
+    public int Quantity { get; init; }
+}
