9c94a31b78639fdfee5ba9ea4ce0010dad1f5b3c
Artem Agafonov <agart@mail.ru>
2025-04-22 21:54:30 +0300

    Use named methods to define links.

--- src/Restaurant.RestApi/Hypertext.cs
+++ src/Restaurant.RestApi/Hypertext.cs
@@ -1,9 +1,57 @@
-﻿namespace Restaurant.RestApi;
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant.RestApi;
 
 internal static class Hypertext
 {
     internal static LinkDto Link(this Uri uri, string rel)
     {
         return new LinkDto { Rel = rel, Href = uri.ToString() };
+    }
+
+    internal static LinkDto LinkToReservations(this IUrlHelper url)
+    {
+        return new UrlBuilder()
+            .WithAction(nameof(ReservationsController.Post))
+            .WithController(nameof(ReservationsController))
+            .BuildAbsolute(url)
+            .Link("urn:reservations");
+    }
+
+    internal static LinkDto LinkToYear(this IUrlHelper url, int year)
+    {
+        return new UrlBuilder()
+            .WithAction(nameof(CalendarController.Get))
+            .WithController(nameof(CalendarController))
+            .WithValues(new { year })
+            .BuildAbsolute(url)
+            .Link("urn:year");
+    }
+
+    internal static LinkDto LinkToMonth(
+        this IUrlHelper url,
+        int year,
+        int month)
+    {
+        return new UrlBuilder()
+            .WithAction(nameof(CalendarController.Get))
+            .WithController(nameof(CalendarController))
+            .WithValues(new { year, month })
+            .BuildAbsolute(url)
+            .Link("urn:month");
+    }
+
+    internal static LinkDto LinkToDay(
+        this IUrlHelper url,
+        int year,
+        int month,
+        int day)
+    {
+        return new UrlBuilder()
+            .WithAction(nameof(CalendarController.Get))
+            .WithController(nameof(CalendarController))
+            .WithValues(new { year, month, day })
+            .BuildAbsolute(url)
+            .Link("urn:day");
     }
 }
