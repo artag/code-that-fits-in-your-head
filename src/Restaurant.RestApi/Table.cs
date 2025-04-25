633b6b83ae0b225df379fa6bd828ddef4ead548f
Artem Agafonov <agart@mail.ru>
2025-04-25 21:54:12 +0300

    Refactor Table to use Visitor.

--- src/Restaurant.RestApi/Table.cs
+++ src/Restaurant.RestApi/Table.cs
@@ -2,29 +2,109 @@
 
 public record Table
 {
-    private Table(bool isStandard, int seats)
+    private readonly ITable _table;
+
+    private Table(ITable table)
     {
-        Seats = seats;
-        IsStandard = isStandard;
-        IsCommunal = !isStandard;
+        _table = table;
     }
 
     public static Table Standard(int seats) =>
-        new Table(true, seats);
+        new Table(new StandardTable(seats));
 
     public static Table Communal(int seats) =>
-        new Table(false, seats);
+        new Table(new CommunalTable(seats));
 
-    public int Seats { get; init; }
-    public bool IsStandard { get; }
-    public bool IsCommunal { get; }
+    public int Seats =>
+        _table.Seats;
+
+    public bool IsStandard =>
+        _table.Accept(new IsStandardVisitor());
+
+    public bool IsCommunal =>
+        !IsStandard;
 
     public Table WithSeats(int newSeats) =>
-        this with { Seats = newSeats };
+        _table.Accept(new CopyAndUpdateSeatsVisitor(newSeats));
 
     internal bool Fits(int quantity) =>
         quantity <= Seats;
 
     internal Table Reserve(int seatsToReserve) =>
         WithSeats(Seats - seatsToReserve);
+
+    private interface ITable
+    {
+        int Seats { get; }
+        T Accept<T>(ITableVisitor<T> visitor);
+    }
+
+    private interface ITableVisitor<T>
+    {
+        T VisitStandard(int seats);
+        T VisitCommunal(int seats);
+    }
+
+    private sealed class StandardTable : ITable
+    {
+        public StandardTable(int seats)
+        {
+            Seats = seats;
+        }
+
+        public int Seats { get; }
+
+        public T Accept<T>(ITableVisitor<T> visitor)
+        {
+            return visitor.VisitStandard(Seats);
+        }
+    }
+
+    private sealed class CommunalTable : ITable
+    {
+        public CommunalTable(int seats)
+        {
+            Seats = seats;
+        }
+
+        public int Seats { get; }
+
+        public T Accept<T>(ITableVisitor<T> visitor)
+        {
+            return visitor.VisitCommunal(Seats);
+        }
+    }
+
+    private sealed class CopyAndUpdateSeatsVisitor : ITableVisitor<Table>
+    {
+        private readonly int _newSeats;
+
+        public CopyAndUpdateSeatsVisitor(int newSeats)
+        {
+            _newSeats = newSeats;
+        }
+
+        public Table VisitStandard(int seats)
+        {
+            return new Table(new StandardTable(_newSeats));
+        }
+
+        public Table VisitCommunal(int seats)
+        {
+            return new Table(new CommunalTable(_newSeats));
+        }
+    }
+
+    private sealed class IsStandardVisitor : ITableVisitor<bool>
+    {
+        public bool VisitStandard(int seats)
+        {
+            return true;
+        }
+
+        public bool VisitCommunal(int seats)
+        {
+            return false;
+        }
+    }
 }
