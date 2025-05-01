bef314943c9d937bd3542feba639b3748350492b
Artem Agafonov <agart@mail.ru>
2025-05-01 12:57:08 +0300

    Change URL format for schedule resource.

--- src/Restaurant.RestApi/ScheduleController.cs
+++ src/Restaurant.RestApi/ScheduleController.cs
@@ -5,6 +5,7 @@ namespace Restaurant.RestApi;
 [Route("schedule")]
 public class ScheduleController : ControllerBase
 {
+    [HttpGet("{year}/{month}/{day}")]
     public ActionResult Get(int _, int __, int ___)
     {
         return new UnauthorizedResult();
